4a48ebc8823a0733053fe85e8116848f27d639ee
Juan Manuel <jcumlop667@g.educaand.es>
2022-11-18 19:01:55 +0100

    Versión 2.5: Función select plenamente funcional y data class MisProductos añadida

--- src/main/kotlin/Gestor.kt
+++ src/main/kotlin/Gestor.kt
@@ -37,44 +37,44 @@ class Gestor private constructor() {
             println("[No existe conexión a la BBDD]")
         }
     }
-    //private val selectAll = "SELECT * FROM productos;"
 
-    //private val miStatement: Statement = conn?.createStatement() ?:
-
-    /*fun todosLosDatos() {
-        val miResultSet = miStatement.executeQuery(selectAll)
-        if (miResultSet != null) {
-
-            println("Todos los productos :")
-            while (miResultSet.next()) {
-                println(miResultSet.getString(1))
-                println(miResultSet.getString(2))
-                println(miResultSet.getString(3))
-                println(miResultSet.getString(4))
-                println(miResultSet.getString(5))
-                println("------------------------------")
-            }
-        }
-    }*/
-    fun select(): MutableMap<String, String> {
-        var resultsMap: MutableMap<String, String> = mutableMapOf()
+    fun select(): MutableList<MisProductos> {
+        var productos: MutableList<MisProductos> = mutableListOf()
 
         if (conn != null) {
-            // Con la conexión creada, vamos a obtener un objeto Statement
-            val st: Statement = conn!!.createStatement()
+            try {
+                conn!!.prepareStatement("SELECT * FROM productos;").use { statement ->
+                    val results = statement.executeQuery()
 
-            // El siguiente paso sería ejecutar una sentencia SQL
-            val sentencia: String = "SELECT * FROM productos;"
-            val results: ResultSet = st.executeQuery(sentencia)
-
-            while (results.next()) {
-                resultsMap["ID"] = results.getString("ID")
-                resultsMap["Nombre"] = results.getString("Nombre")
-                resultsMap["Precio"] = results.getInt("Precio").toString()
-                resultsMap["Cantidad"] = results.getInt("Cantidad").toString()
-                resultsMap["Descripcion"] = results.getString("Descripcion")
+                    while (results.next()) {
+                        val id = results.getString("ID")
+                        val nombre = results.getString("Nombre")
+                        val precio = results.getInt("Precio")
+                        val cantidad = results.getInt("Cantidad")
+                        val descripcion = results.getString("Descripcion")
+                        productos.add(MisProductos(id, nombre, precio, cantidad, descripcion))
+                    }
+                }
+            } catch (e: SQLException) {
+                printSQLException(e)
             }
         }
-        return resultsMap
+        return productos
+    }
+
+    private fun printSQLException(ex: SQLException) {
+        for (e in ex) {
+            if (e is SQLException) {
+                e.printStackTrace(System.err)
+                System.err.println("SQLState: " + e.sqlState)
+                System.err.println("Error Code: " + e.errorCode)
+                System.err.println("Message: " + e.message)
+                var t = ex.cause
+                while (t != null) {
+                    println("Cause: $t")
+                    t = t.cause
+                }
+            }
+        }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ fun main() {
     val dbInstance: Gestor = Gestor.getInstance()
     dbInstance.conectarBBDD()
     var selectAll = dbInstance.select()
-    selectAll.forEach { t, u ->
-        println("$t : $u")
-    }
+    selectAll.forEach { println(it) }
+    dbInstance.desconectar()
 }
